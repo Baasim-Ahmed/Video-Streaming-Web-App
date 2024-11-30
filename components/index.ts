7ee38b3c1e89c5f4f6967e76ac6ba866a83a8842
Baasim Ahmed Rind <k225029@nu.edu.pk>
2024-11-30 21:18:13 +0500

    first commit

---
+++ components/index.ts
@@ -0,0 +1,7 @@
+import { InfoModal } from "./InfoModal";
+import { PlayButton } from "./Play";
+import { MovieList } from "./Movie";
+import { Billboard } from "./Billboard";
+import { CustomInput } from "./CustomInput";
+import { Navbar } from "./navbar";
+export { CustomInput, Navbar, Billboard, MovieList, PlayButton, InfoModal };
